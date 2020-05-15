f4ced415cec9f15ac8a4868db75f5005229aa9c8
celestebarrios <celestebarrios35@gmail.com>
2020-05-15 11:50:06 -0500

    Edits

---
+++ parkinggarage.py
@@ -0,0 +1,33 @@
+class ParkingGarage():
+    D = {}
+    def __init__(self, tickets, parking_Spaces, current_Ticket)
+        self.tickets = tickets
+        self.parking_Spaces = parking_Spaces
+        self.current_Ticket = current_Ticket
+
+    def Parking(self, parking_Spaces):
+        self.parking_Spaces= list()
+        print(self.parking_Spaces)
+    
+    def Add_Ticket(self, tickets)
+        Tickets = input('Would you like to purchase a ticket? ')
+        self.tickets.append(Tickets)
+
+    def Pay(self, current_Ticket):
+        pay = input('Have A Nice Day')
+    
+    
+        
+  
+
+def Run():
+        while True:
+        User_Input = input('Purchase ticket?')
+        if User_Input.lower() == 'park':
+            User_Input.addToShoppingBag()
+        if User_Input.lower() == 'pay':
+            User_Input.showShoppingBag()
+        if User_Input.lower() == 'leave':
+            User_Input.showShoppingBag()
+            break
+Run()
