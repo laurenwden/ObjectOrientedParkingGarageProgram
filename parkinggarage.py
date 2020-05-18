8f27e442487560e28ecf9735a418b8859e7f9462
laurenwden <laurenm.weber@outlook.com>
2020-05-17 18:50:59 -0600

    Final final submission after debugging, styling and adding responses for invalid user input. -LW

--- parkinggarage.py
+++ parkinggarage.py
@@ -1,89 +1,95 @@
 class ParkingGarage():
-    def __init__(self, Tickets_Available, parking_Spaces, current_Ticket_D):
-        self.Tickets_Available = Tickets_Available
-        self.parking_Spaces = parking_Spaces
-        self.current_Ticket_D = current_Ticket_D
+    def __init__(self, tickets_available, parking_spaces, current_ticket_d):
+        self.tickets_available = tickets_available
+        self.parking_spaces = parking_spaces
+        self.current_ticket_d = current_ticket_d
 
     # Method for taking a ticket to enter the garage
-
     def takeTicket(self):
-        if len(self.parking_Spaces) == 0:
-            print("I'm sorry, there are no available spaces! Have a nice day.")
+        if len(self.parking_spaces) == 0:
+            print("\nI'm sorry, there are no available spaces! Have a nice day.")
             return
         else:
             response = input(
-                "Would you like to pay now? Type yes for now or no for later.")
+                "\nWould you like to pay now? Type yes for now or no for later.")
+
             if response == 'yes':
-                payment = input(
-                    "Your total is $10. Please enter payment amount once you have submitted your payment.")
+                payment = input("\nYour total is $10. Please enter payment amount once you have submitted your payment.")
 
                 if payment == '10':
-                    working_ticket = self.Tickets_Available.pop(0)
+                    working_ticket = self.tickets_available.pop(0)
                     print(
-                        f"Your ticket number is {working_ticket}. Thank you, please take your ticket and enter the garage.")
-                    self.current_Ticket_D[working_ticket] = True
-                    self.parking_Spaces.pop(0)
-                    print('Thank you, Have A Nice Day')
+                        f"\nYour ticket number is {working_ticket}. Thank you, please take your ticket and enter the garage.")
+                    self.current_ticket_d[working_ticket] = True
+                    self.parking_spaces.pop(0)
                 else:
                     payment = input(
-                        "Your total is $10. Please enter payment amount once you have submitted your payment.")
-                    working_ticket = self.Tickets_Available.pop(0)
-                    print(
-                        f"Your ticket number is {working_ticket}. Thank you, please take your ticket and enter the garage.")
-                    self.current_Ticket_D[working_ticket] = True
-                    self.parking_Spaces.pop(0)
-                    print('Thank you, Have A Nice Day')
+                        "\nYour total is $10. Please enter payment amount once you have submitted your payment.")
 
+                    working_ticket = self.tickets_available.pop(0)
+
+                    print(
+                        f"\nYour ticket number is {working_ticket}. Thank you, please take your ticket and enter the garage.")
+
+                    self.current_ticket_d[working_ticket] = True
+                    self.parking_spaces.pop(0)
             if response == 'no':
-                #working_ticket isn't a list
-                working_ticket = self.Tickets_Available.pop(0)
-                print(
-                    f"Your ticket number is {working_ticket}. Thank you, please take your ticket and enter the garage.")
-                self.current_Ticket_D[working_ticket] = False
-                self.parking_Spaces.pop(0)
-                print(working_ticket)
+                working_ticket = self.tickets_available.pop(0)
+                print(f"\nYour ticket number is {working_ticket}. Thank you, please take your ticket and enter the garage.")
+                self.current_ticket_d[working_ticket] = False
+                self.parking_spaces.pop(0)
+
+        #Method to pay before leaving
+    def payForParking(self):
+        ticket_number = int(input("\nPlease enter your ticket number."))
+        if ticket_number in self.current_ticket_d:
+            if self.current_ticket_d[ticket_number] == True:
+                print("\nYou have already paid.")
+            else:
+                payment = input(
+                    "\nYour total is $10. Please enter amount once you have submitted your payment.")
+
+                if payment != '10':
+                    print("\nThat is not the correct amount.")
+                else:
+                    self.current_ticket_d[ticket_number] = True
+                    self.tickets_available.append(ticket_number)
+                    self.parking_spaces.append(1)
+                    print(
+                        '\nThank you! You have 15 minutes to leave the garage. Have a nice day!')
+        else:
+            print("\nThat is not a valid ticket number.")
 
     #Method to initiate leaving the garage
     def leaveGarage(self):
-        Ticket_Number = int(input("What is your ticket number?"))
-        User_Payment = self.current_Ticket_D.get(Ticket_Number)
+        ticket_number = int(input("\nWhat is your ticket number?"))
 
-        if User_Payment == False:
-            self.Input_Payment(Ticket_Number)
-        del self.current_Ticket_D[Ticket_Number]
-        self.Add_to_List(Ticket_Number)
-        print('Thank you, Have A Nice Day')
+        if ticket_number in self.current_ticket_d:       
+            payment = self.current_ticket_d.get(ticket_number)
 
-    def Input_Payment(self, Ticket_Number):
-        if input("Your total is $10. Please enter payment amount once you have submitted your payment.") != '10':
-            print("FEED MEEE IM HUNGRY!")
+            if payment == False:
+                print("\nYou must pay before exiting.")
+            else:
+                del self.current_ticket_d[ticket_number]
+                self.tickets_available.append(ticket_number)
+                self.parking_spaces.append(1)
+                print('Thank you! You have 15 minutes to leave the garage. Have a nice day!')
+
         else:
-            self.current_Ticket_D[Ticket_Number] = True
-
-    #Method to pay before leaving
-    def payForParking(self):
-        ticket_number = input("What is your ticket number?")
-        self.Input_Payment(ticket_number)
-        print('Thank you, Have A Nice Day')
-
-    #Adding items back to available tickets and parking spaces
-    def Add_to_List(self, Ticket_Number):
-        self.Tickets_Available.append(Ticket_Number)
-        self.parking_Spaces.append(1)
-
+            print("\nThat is not a valid ticket number.")
 
 def Run():
-        variable = ParkingGarage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [
+        action = ParkingGarage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [
                                  1, 1, 1, 1, 1, 1, 1, 1, 1, 1], {})
+
         while True:
-            User_Input = input('What would you like to do? Park, pay or leave?')
-            if User_Input.lower() == 'park':
-                variable.takeTicket()
-            if User_Input.lower() == 'pay':
-                variable.payForParking()
-            if User_Input.lower() == 'leave':
-                variable.leaveGarage()
-            
+            user_input = input('What would you like to do? Park, pay or leave?')
+            if user_input.lower() == 'park':
+                action.takeTicket()
+            if user_input.lower() == 'pay':
+                action.payForParking()
+            if user_input.lower() == 'leave':
+                action.leaveGarage()           
                 
 Run()
 
